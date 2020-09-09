2516fe008c9e041e339a50d5a2108636ecd5c93c
jordan meta <irnawatyully@gmail.com>
2020-09-09 17:06:18 +0800

    flatten day

--- web-client-user/src/components/commons/mahasiswa/Schedule.svelte
+++ web-client-user/src/components/commons/mahasiswa/Schedule.svelte
@@ -53,17 +53,32 @@
         });
         items = [];
         raws.forEach(item => {
-          const hari = formatDay(item.hariKul);
-          let group = items.find(gr => gr.hari == hari);
-          if (!group) {
-            group = {
-              hari,
-              items: []
-            };
-            items.push(group);
+          if (items.length == 0) {
+            // if empty list, push day of this item
+            const itemDay = formatDay(item.hariKul);
+            items.push({
+              separator: true,
+              text: itemDay,
+              idKelas: itemDay // fake id for iteration key
+            })
+            items.push(item);
+            return;
           }
-          group.items.push(item);
-        });
+          // if day of last item is different from current item,
+          // add separator
+          const n = items.length;
+          const prevItem = items[n - 1];
+          const prevItemDay = formatDay(prevItem.hariKul);
+          const currentItemDay = formatDay(item.hariKul);
+          if (prevItemDay != currentItemDay) {
+            items.push({
+              separator: true,
+              text: currentItemDay,
+              idKelas: currentItemDay // fake id for iteration key
+            })
+          }
+          items.push(item);
+        })
         console.log(items);
         networkStatus = 'success';
       })
@@ -82,8 +97,8 @@
   $: loadSchedule({ idMahasiswa, idPeriode });
 </script>
 
-<div class="bg-white mb-2 py-4 px-4">
-  <div class="text-lg font-bold">Jadwal Perkuliahan</div>
+<div class="text-3xl font-black my-6">Jadwal Perkuliahan</div>
+<div class="w-64 mb-6">
   <JoSelect
     options={optionsPeriode}
     emptyLabel='Pilih Periode'
@@ -94,28 +109,25 @@
 <JoAsyncContent {networkStatus}>
   <div slot="success" class="bg-white">
     <ul>
-      {#each items as group (group.hari)}
-        <li class="p-4 flex">
-          <div class="font-semibold w-16 ">{group.hari}</div>
-          <ul class="ml-2 pl-2 border-l-2 border-teal-600 flex-grow">
-            {#each group.items as item (item.idKelas)}
-              <li class="pl-4 py-4 border-b border-gray-300 w-full">
-                <div class="flex">
-                  <div class="flex flex-col">
-                    <a href={`/#/app/ilkom/mks/${item.idMk}`} class="font-bold underline">{item.namaMk}</a>
-                    <div>
-                      <a class="font-semibold underline" href={`/#/app/ilkom/kelas/${item.idKelas}/schedule`}>kelas {item.label}</a>, 
-                      <a class="font-semibold underline" href={`/#/app/ilkom/dosens/${item.idDosen}/info`}>
-                        {item.namaDosen}
-                      </a>
-                    </div>
-                    <div>{item._schedule}, {item.ruangan}</div>
-                  </div>
+      {#each items as item (item.idKelas)}
+        {#if item.separator}
+          <li class="font-bold mt-8 text-2xl">{item.text}</li>
+        {:else}
+          <li class="p-4 border-b-2 border-dashed border-gray-400 w-full">
+            <div class="flex">
+              <div class="flex flex-col">
+                <div class="text-lg">
+                  <a href={`/#/app/ilkom/mks/${item.idMk}`} class="font-bold underline mr-4">{item.namaMk}</a>
+                  <a class="mr-4 font-semibold underline" href={`/#/app/ilkom/kelas/${item.idKelas}/schedule`}>kelas {item.label}</a>, 
+                  <a class="mr-4 font-semibold underline" href={`/#/app/ilkom/dosens/${item.idDosen}/info`}>
+                    {item.namaDosen}
+                  </a>
                 </div>
-              </li>
-            {/each}
-          </ul>
-        </li>
+                <div>{item._schedule}, {item.ruangan}</div>
+              </div>
+            </div>
+          </li>
+        {/if}
       {/each}
     </ul>
   </div>

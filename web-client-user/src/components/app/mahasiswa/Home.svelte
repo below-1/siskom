70dd9dda49143b9f483e5008dc1f548755e6dc2e
jordan meta <irnawatyully@gmail.com>
2020-09-15 23:37:05 +0800

    Ilkom Menus

--- web-client-user/src/components/app/mahasiswa/Home.svelte
+++ web-client-user/src/components/app/mahasiswa/Home.svelte
@@ -8,6 +8,10 @@
   import FaCheckDouble from 'svelte-icons/fa/FaCheckDouble.svelte'
   import FaFileExport from 'svelte-icons/fa/FaFileExport.svelte'
   import FaFileImport from 'svelte-icons/fa/FaFileImport.svelte'
+  import FaUserSecret from 'svelte-icons/fa/FaUserSecret.svelte'
+  import FaBook from 'svelte-icons/fa/FaBook.svelte'
+  import FaBezierCurve from 'svelte-icons/fa/FaBezierCurve.svelte'
+  import FaCalendarDay from 'svelte-icons/fa/FaCalendarDay.svelte'
 
   const personalMenus = [
     { label: 'Info', path: '/#/app/me_mhs/info', icon: IoIosContact },
@@ -18,7 +22,12 @@
     { label: 'KHS', path: '/#/app/me_mhs/khs', icon: FaFileImport }
   ]
 
+
   const ilkomMenus = [
+    { label: 'Dosen', path: '/#/app/ilkom/dosens', icon: FaUserSecret },
+    { label: 'Mata Kuliah', path: '/#/app/ilkom/mks', icon: FaBook },
+    { label: 'Kelas', path: '/#/app/ilkom/kelas', icon: FaBezierCurve },
+    { label: 'Periode', path: '/#/app/ilkom/periodes', icon: FaCalendarDay },
   ]
 
   let id = null;
@@ -45,7 +54,19 @@
     </a>
   {/each}
 </div>
-<h2 class="text-3xl text-gray-700 font-black mb-4 text-center">Jurusan</h2>
-
+<h2 class="text-3xl text-gray-700 font-black mb-4 text-center">Prodi</h2>
+<div class="grid grid-cols-2 gap-3 mb-8 p-4 md:p-0 md:grid-cols-3">
+  {#each ilkomMenus as menu (menu.label)}
+    <a 
+      href={menu.path}
+      class="p-8 bg-gray-100 rounded flex flex-col items-center justify-center font-bold text-gray-600 hover:text-gray-900"
+    >
+      <div class="h-20 w-20">
+        <svelte:component this={menu.icon}></svelte:component>
+      </div>
+      <div class="text-2xl my-2">{menu.label}</div>
+    </a>
+  {/each}
+</div>
 <!-- <DetailMahasiswa {id} prefix="/app/me_mhs">
 </DetailMahasiswa> -->
